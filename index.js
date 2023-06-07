1999153d4f65f00d52dbdfa1345c4bf4b02e4c2f
Rahul Saha <rahulsaha87928@gmail.com>
2023-06-08 00:56:06 +0600

    post class data

--- index.js
+++ index.js
@@ -29,12 +29,62 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const db = client.db('summerCamp');
+    const classCollection = db.collection('class');
+    const usersCollection = db.collection('users');
+
+    //users data
+
+    // app.get('/users', async (req, res) => {
+    //     const result = await usersCollection.find().toArray();
+    //     res.send(result);
+    //   });
+
+
+    // app.post('/users', async (req, res) => {
+    //     const user = req.body;
+    //     const query = { email: user.email }
+    //     const existingUser = await usersCollection.findOne(query);
+  
+    //     if (existingUser) {
+    //       return res.send({ message: 'user already exists' })
+    //     }
+  
+    //     const result = await usersCollection.insertOne(user);
+    //     res.send(result);
+    //   });
+
+    //get class data
+    // app.get('/class', async (req, res) => {
+    //     const result = await classCollection.find().toArray();
+    //     res.send(result);
+    //   });
+
+    // Post class data
+    app.post('/class', async (req, res) => {
+      const addedItem = req.body;  
+      const result = await classCollection.insertOne(addedItem);
+      res.send(result);
+    });
+  
+
+
+
+
+
+
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
