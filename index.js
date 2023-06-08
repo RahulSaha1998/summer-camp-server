b7c1ea21dcca6a677ec8eb68f664151762365f7b
Rahul Saha <rahulsaha87928@gmail.com>
2023-06-08 12:55:59 +0600

    update to admin

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 require('dotenv').config()
 
@@ -55,7 +55,37 @@ async function run() {
         res.send(result);
       });
 
+      app.patch('/users/admin/:id', async(req, res)=>{
+        const id = req.params.id;
+        const filter = { _id: new ObjectId(id) };
+        const updateDoc = {
+          $set: {
+            role: 'admin'
+          },
+        };
+  
+        const result = await usersCollection.updateOne(filter, updateDoc);
+        res.send(result);
+      })
+
+    //   app.patch('/users/instructor/:id', async(req, res)=>{
+    //     const id = req.params.id;
+    //     const filter = { _id: new ObjectId(id) };
+    //     const updateDoc = {
+    //       $set: {
+    //         role: 'instructor'
+    //       },
+    //     };
+  
+    //     const result = await usersCollection.updateOne(filter, updateDoc);
+    //     res.send(result);
+    //   })
+
+
+
+
     //get class data
+    
     app.get('/class', async (req, res) => {
         const result = await classCollection.find().toArray();
         res.send(result);
