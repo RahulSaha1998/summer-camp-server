6202a0e6ba42bc98b4774520c284cef9fc454987
Rahul Saha <rahulsaha87928@gmail.com>
2023-06-08 12:56:39 +0600

    update to instructor

--- index.js
+++ index.js
@@ -68,18 +68,18 @@ async function run() {
         res.send(result);
       })
 
-    //   app.patch('/users/instructor/:id', async(req, res)=>{
-    //     const id = req.params.id;
-    //     const filter = { _id: new ObjectId(id) };
-    //     const updateDoc = {
-    //       $set: {
-    //         role: 'instructor'
-    //       },
-    //     };
+      app.patch('/users/instructor/:id', async(req, res)=>{
+        const id = req.params.id;
+        const filter = { _id: new ObjectId(id) };
+        const updateDoc = {
+          $set: {
+            role: 'instructor'
+          },
+        };
   
-    //     const result = await usersCollection.updateOne(filter, updateDoc);
-    //     res.send(result);
-    //   })
+        const result = await usersCollection.updateOne(filter, updateDoc);
+        res.send(result);
+      })
 
 
 
